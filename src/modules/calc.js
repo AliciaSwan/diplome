232195294bfccbad9dcebf649ac34ae475b71665
AliciaSwan <ange2002@mail.ru>
2019-08-13 02:49:10 +0200

    calculator

--- src/modules/calc.js
+++ src/modules/calc.js
@@ -1,23 +1,48 @@
 const calc = () => {
     const total = document.getElementById('price-total'),
-        m1 = document.getElementById('m1'),
-        m2 = document.getElementById('m2'),
-        m3 = document.getElementById('m3'),
-        m4 = document.getElementById('m4'),
-        cardOrder = document.getElementById('card_order'),
-        clubName = document.querySelector('.club');
+        cardOrder = document.getElementById('card_order');
+
+    //получаем данные по умочанию
+    const getCheckedRadioValue = (name) => {
+	    const elements = document.getElementsByName(name);	
+	    for (let i=0, len = elements.length; i<len; ++i){
+            if (elements[i].checked) {
+                return elements[i].value;
+            }
+        }     
+    };
+    
+    const getValue = (e) =>{
+        const target = e.target,
+            clubValue = getCheckedRadioValue('club-name'),
+            timeValue = getCheckedRadioValue('card-type');
 
 
+        if(clubValue == "mozaika"){
+            if( timeValue == 1 || target.value == 1){
+            total.textContent = 1999 + "₽";
+            }else if( timeValue == 6 || target.value == 6){
+                total.textContent = 9999 + "₽";
+            }else if( timeValue == 9 || target.value == 9){
+                total.textContent = 13999 + "₽";
+            }else if(timeValue == 12 || target.value == 12){
+                total.textContent = 19999 + "₽";
+            }
+        } 
+        if(clubValue == "schelkovo"){
+            if( timeValue == 1 || target.value == 1){
+                total.textContent = 2999 + "₽";
+            }else if( timeValue == 6 || target.value == 6){
+                total.textContent = 14999 + "₽";
+            }else if( timeValue == 9 || target.value == 9){
+                total.textContent = 21999 + "₽";
+            }else if(timeValue == 12 || target.value == 12){
+                total.textContent = 24999 + "₽";
+            }
+        }
+    };
 
+    cardOrder.addEventListener('change', getValue);
 
-    // const countSum = () => {
-    //     let total = 0,
-    //     countValue = 1,
-    //     dayValue = 1;
-    //     const typeValue = calcType.options[calcType.selectedIndex].value,
-    //         squareValue = +calcSquare.value;
-    //     cardOrder.addEventListener('click', () => {
-    //     // if()
-    //     });
-    // };
 };
+export default calc;
